746b3427af0f21f82b10413cc8f460282c7261a0
Roy Lopez <roylopezdev@gmail.com>
2023-07-27 13:51:06 -0300

    create search components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,24 +1,12 @@
-import React from "react";
+import NavBar from "@/app/components/NavBar";
+import Link from "next/link";
 
 const RestaurantMenu = () => {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
         {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <a href="" className="font-bold text-gray-700 text-2xl">
-            {" "}
-            OpenTable{" "}
-          </a>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
+        <NavBar />
         {/* NAVBAR */} {/* HEADER */}
         <div className="h-96 overflow-hidden">
           <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
@@ -32,14 +20,12 @@ const RestaurantMenu = () => {
           <div className="bg-white w-[100%] rounded p-3 shadow">
             {/* RESAURANT NAVBAR */}
             <nav className="flex text-reg border-b pb-2">
-              <a href="" className="mr-7">
-                {" "}
-                Overview{" "}
-              </a>
-              <a href="" className="mr-7">
-                {" "}
-                Menu{" "}
-              </a>
+              <Link href="/restaurant/milestone-grill" className="mr-7">
+                Overview
+              </Link>
+              <Link href="/restaurant/milestone-grill/menu" className="mr-7">
+                Menu
+              </Link>
             </nav>
             {/* RESAURANT NAVBAR */} {/* MENU */}
             <main className="bg-white mt-5">
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,47 +1,17 @@
 import React from "react";
+import NavBar from "@/app/components/NavBar";
+import RestaurantNav from "@/app/restaurant/components/RestaurantNav";
+import Headers from "@/app/restaurant/components/Headers";
 
 const RestaurantDetails = () => {
   return (
     <main className="bg-gray-100 min-h-screen w-screen">
       <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <a href="" className="font-bold text-gray-700 text-2xl">
-            {" "}
-            OpenTable{" "}
-          </a>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
-        {/* NAVBAR */} {/* HEADER */}
-        <div className="h-96 overflow-hidden">
-          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-            <h1 className="text-7xl text-white captitalize text-shadow text-center">
-              Milestones Grill (Toronto)
-            </h1>
-          </div>
-        </div>
-        {/* HEADER */} {/* DESCRIPTION PORTION */}
+        <NavBar />
+        <Headers /> {/* DESCRIPTION PORTION */}
         <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
           <div className="bg-white w-[70%] rounded p-3 shadow">
-            {/* RESAURANT NAVBAR */}
-            <nav className="flex text-reg border-b pb-2">
-              <a href="" className="mr-7">
-                {" "}
-                Overview{" "}
-              </a>
-              <a href="" className="mr-7">
-                {" "}
-                Menu{" "}
-              </a>
-            </nav>
-            {/* RESAURANT NAVBAR */} {/* TITLE */}
+            <RestaurantNav /> {/* TITLE */}
             <div className="mt-4 border-b pb-6">
               <h1 className="font-bold text-6xl">Milesstone Grill</h1>
             </div>
