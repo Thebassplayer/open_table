073768236c86bb8257c8738859db2bea7468a400
Roy Lopez <roylopezdev@gmail.com>
2023-08-01 10:05:10 -0300

    implement fetch filtered data @ /search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -13,8 +13,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getRestaurantsAtCity = (location: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    slug: true,
+    cuissine: true,
+    location: true,
+  };
   if (!location) {
-    return prisma.restaurant.findMany({});
+    return prisma.restaurant.findMany({ select });
   }
   return prisma.restaurant.findMany({
     where: {
@@ -24,6 +33,7 @@ const getRestaurantsAtCity = (location: string | undefined) => {
         },
       },
     },
+    select,
   });
 };
 
@@ -43,9 +53,15 @@ const Search = async ({
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6">
-          {/* {restaurants.map(restaurant => (
-              <RestaurantCard key={restaurant.id} />
-            ))} */}
+          {!restaurants.length ? (
+            <div className="text-2xl font-bold mt-4 mb-2">
+              Sorry, there is no restaurant registered in {location}
+            </div>
+          ) : (
+            restaurants.map(restaurant => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))
+          )}
         </div>
       </div>
     </>
